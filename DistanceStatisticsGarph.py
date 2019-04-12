b9cff33a1faab985705a4dd15c6fb029c34e5c79
dc-angel <44229085+dc-angel@users.noreply.github.com>
2019-04-13 00:26:29 +0300

    Update DistanceStatisticsGarph.py

--- DistanceStatisticsGarph.py
+++ DistanceStatisticsGarph.py
@@ -37,34 +37,51 @@ def plotGraph(distList):
     # Plot
     ply.plot(fig, filename='./data/LevenshteinDistGraph.html')
 
-def hamming2(s1, s2):
-    """Calculate the Hamming distance between two bit strings"""
-    assert len(s1) == len(s2)
-    return sum(c1 != c2 for c1, c2 in zip(s1, s2))
 
 
-def Compute_dist_by_shifts_bits_using_liventshtain(originalKeyStr, monitoredaKeysStr):
+# def hamming2(s1, s2):
+#     """Calculate the Hamming distance between two bit strings"""
+#     assert len(s1) == len(s2)
+#     return sum(c1 != c2 for c1, c2 in zip(s1, s2))
+
+
+
+
+def Compute_dist_by_shifts_bits_using_levenshtein(originalKeyStr, monitoredKeysStr):
+    '''
+
+    :param originalKeyStr: the original key
+    :param monitoredKeysStr: the monitored data in which we have to find string that matches 
+        the original key with minimum levenshtein 
+    :return listOfAllDicts: list of dicts with the  distance, flips, deletions , insertion {"DIST": _; "I": _; "F": _, "D"_X}
+    :return distValuesList: 
+    :return numberOfAvalibleKeys: 
+
+    '''
     listOfAllDicts = []
     distValuesList = []
-    numberOfAvalibleKeys = len(monitoredaKeysStr) - len(originalKeyStr) + 1
-    # 1xxxx =>5
+    
+    #calculate how many keys can be in the given monitoredKeysStr
+    # xxxxx =>5
     # yyyy =>4
     # 5-4=1
-    originalKeyLen=len(originalKeyStr)
+    #but there is 2 options so we add +1
+    numberOfAvalibleKeys = len(monitoredKeysStr) - len(originalKeyStr) + 1
+    originalKeyLen = len(originalKeyStr)
     for i in xrange(0, numberOfAvalibleKeys):
         print i
-        tempStr = monitoredaKeysStr[i: i+originalKeyLen]
+        tempStr = monitoredKeysStr[i: i+originalKeyLen]
 
-        dict = GabiFuncs.levenshtein_edit_dist(originalKeyStr, tempStr, show_strings=False)
+        #dict = GabiFuncs.levenshtein_edit_dist(originalKeyStr, tempStr, show_strings=False)
 
-        # dict=({"DIST":1,"I":2,"D":3,"F":4},)
-        # if hamming2(originalKeyStr,tempStr) < len(originalKeyStr)/8:
-        #     dict = GabiFuncs.levenshtein_edit_dist(originalKeyStr, tempStr, show_strings=False)
-        # else:
-        #     dict[0]["DIST"]=len(originalKeyStr)/8
-        #     dict[0]["I"] = len(originalKeyStr) / 8
-        #     dict[0]["F"] = len(originalKeyStr) / 8
-        #     dict[0]["D"] = len(originalKeyStr) / 8
+        dict=({"DIST":1,"I":2,"D":3,"F":4},)
+        if GabiFuncs.hamming_dist(originalKeyStr,tempStr) < len(originalKeyStr)/8:
+            dict = GabiFuncs.levenshtein_edit_dist(originalKeyStr, tempStr, show_strings=False)
+        else:
+            dict[0]["DIST"]=len(originalKeyStr)/8
+            dict[0]["I"] = len(originalKeyStr) / 8
+            dict[0]["F"] = len(originalKeyStr) / 8
+            dict[0]["D"] = len(originalKeyStr) / 8
 
         listOfAllDicts.append(dict[0])
         print dict[0]
@@ -76,6 +93,14 @@ def Compute_dist_by_shifts_bits_using_liventshtain(originalKeyStr, monitoredaKey
 
 
 def FinedAllMinimumsDicts(listOfAllDicts, distValuesList, numberOfAvalibleKeys, len):
+    """
+    this function 
+    :param listOfAllDicts: 
+    :param distValuesList: 
+    :param numberOfAvalibleKeys: 
+    :param len: 
+    :return: 
+    """
     listOfAllDictsWithMinimumDist = []
     allMinmunsIndex = []
     distValuesListNumpy = np.array(distValuesList)
@@ -103,11 +128,18 @@ def FinedAllMinimumsDicts(listOfAllDicts, distValuesList, numberOfAvalibleKeys, 
 
 
 
-def CaclulateAvg(listOfDicts):
-    insertionList = list(map(lambda x: x['I'], listOfDicts))
-    deletionList = list(map(lambda x: x['D'], listOfDicts))
-    flipsList = list(map(lambda x: x['F'], listOfDicts))
-    distsList = list(map(lambda x: x['F'], listOfDicts))
+def CaclulateAvg(listOfAllDictsWithMinimumDist):
+    '''
+    
+    :param listOfAllDictsWithMinimumDist: list of dictionaries that holds computeation of all the minimum errors in monitored data string collected from cache LLC side channel using levenshtein distance
+        (error rate includes distance, flips, deletions , insertion) 
+    :return: dict with the  average distance, average flips, average deletions , average insertion
+        {"insertionAVG": _; "deletionAVG": _; "flipsAVG": _, "LevDistAVG" _}
+    '''
+    insertionList = list(map(lambda x: x['I'], listOfAllDictsWithMinimumDist))
+    deletionList = list(map(lambda x: x['D'], listOfAllDictsWithMinimumDist))
+    flipsList = list(map(lambda x: x['F'], listOfAllDictsWithMinimumDist))
+    distsList = list(map(lambda x: x['F'], listOfAllDictsWithMinimumDist))
 
     insertionListNumpy = np.array(insertionList)
     deletionListNumpy = np.array(deletionList)
@@ -121,12 +153,23 @@ def CaclulateAvg(listOfDicts):
     result["LevDistAVG"] = np.mean(distsListNumpy)
     return result
 
-def ComputeAvgOfAllMinumumsInMonitoredKeys(originalKeyStr, monitoredaKeysStr):
+
+
+def ComputeAvgOfAllMinumumsInMonitoredKeys(originalKeyStr, monitoredKeysStr):
+    '''
+    This is the main function which computes 
+    the average error in monitored data string collected from cache LLC side channel using levenshtein distance
+    error rate includes average distance, average flips, average deletions , average insertion 
+    and plots graph 
+    :param originalKeyStr: the original key
+    :param monitoredKeysStr: the monitored data in which we have to find string that matches 
+        the original key with minimum levenshtein 
+    :return: None
+    '''
+    
     conclusiton = {}
 
-    listOfAllDicts, distValuesList, numberOfAvalibleKeys = Compute_dist_by_shifts_bits_using_liventshtain(
-        originalKeyStr, monitoredaKeysStr)
-
+    listOfAllDicts, distValuesList, numberOfAvalibleKeys = Compute_dist_by_shifts_bits_using_levenshtein(originalKeyStr, monitoredKeysStr)
 
     str = "numberOfAvalibleKeys ="
     print str
@@ -143,8 +186,7 @@ def ComputeAvgOfAllMinumumsInMonitoredKeys(originalKeyStr, monitoredaKeysStr):
     print allMinmunsIndex
     FILE_FOR_MORE_INFORMATION.write("%s\n" % allMinmunsIndex)
 
-
-    conclusiton = CaclulateAvg(listOfAllDictsWithMinimumDist)
+    conclusiton = CaclulateAvg(listOfAllDictsWithMinimumDist) 
 
     str = "THE AVRAGE OF NOISE IS:"
     print str
@@ -156,51 +198,47 @@ def ComputeAvgOfAllMinumumsInMonitoredKeys(originalKeyStr, monitoredaKeysStr):
 
 
 
-
-
-
-FILE_FOR_ALL_DICTS = open('./data/listOfAllDicts.txt', 'w')
-FILE_FOR_ALL_MINIMUM_DICTS = open('./data/listOfAllDictsWithMinimumDist.txt', 'w')
-FILE_FOR_MORE_INFORMATION = open('./data/moreInfo.txt', 'w')
-
-
-
-originalKeyFile = open(defines.FILE_ORIGINAL_KEY, "rt") #open file
-# originalKeyStr = originalKeyFile.readline()
-originalKeyStr = originalKeyFile.read().replace('\n','').replace(' ','').replace(',','')
-originalKeyFile.close()
-str = "originalKeyStr len= "
-print str
-FILE_FOR_MORE_INFORMATION.write("%s\n" % str)
-print len(originalKeyStr)
-FILE_FOR_MORE_INFORMATION.write("%s\n" % len(originalKeyStr))
-
-
-monitoredaKeysFile = open(defines.FILE_SAMPLED_DATA, "rt") #open file
-# monitoredaKeysStr = monitoredaKeysFile.readline()
-monitoredaKeysStr = monitoredaKeysFile.read().replace('\n','').replace(' ','').replace('[','').replace(']','').replace(',','')
-monitoredaKeysFile.close()
-
-str = "monitoredaKeysStr len= "
-print str
-FILE_FOR_MORE_INFORMATION.write("%s\n" % str)
-print len(monitoredaKeysStr)
-FILE_FOR_MORE_INFORMATION.write("%d\n" % (len(monitoredaKeysStr)))
-
-
-monitoredaKeysStr = monitoredaKeysStr[0:len(monitoredaKeysStr)/1]
-str = "new monitoredaKeysStr len= "
-print str
-FILE_FOR_MORE_INFORMATION.write("%s\n" % str)
-print len(monitoredaKeysStr)
-FILE_FOR_MORE_INFORMATION.write("%d\n" % (len(monitoredaKeysStr)/1))
-
-
-result = ComputeAvgOfAllMinumumsInMonitoredKeys(originalKeyStr, monitoredaKeysStr)
-
-FILE_FOR_ALL_DICTS.close()
-FILE_FOR_ALL_MINIMUM_DICTS.close()
-FILE_FOR_MORE_INFORMATION.close()
+if __name__ == "main":
+    FILE_FOR_ALL_DICTS = open('./data/listOfAllDicts.txt', 'w')
+    FILE_FOR_ALL_MINIMUM_DICTS = open('./data/listOfAllDictsWithMinimumDist.txt', 'w')
+    FILE_FOR_MORE_INFORMATION = open('./data/moreInfo.txt', 'w')
+        
+    originalKeyFile = open(defines.FILE_ORIGINAL_KEY, "rt") #open file
+    # originalKeyStr = originalKeyFile.readline()
+    originalKeyStr = originalKeyFile.read().replace('\n','').replace(' ','').replace(',','')
+    originalKeyFile.close()
+    str = "originalKeyStr len= "
+    print str
+    FILE_FOR_MORE_INFORMATION.write("%s\n" % str)
+    print len(originalKeyStr)
+    FILE_FOR_MORE_INFORMATION.write("%s\n" % len(originalKeyStr))
+    
+    
+    monitoredaKeysFile = open(defines.FILE_SAMPLED_DATA, "rt") #open file
+    # monitoredKeysStr = monitoredaKeysFile.readline()
+    monitoredKeysStr = monitoredaKeysFile.read().replace('\n','').replace(' ','').replace('[','').replace(']','').replace(',','')
+    monitoredaKeysFile.close()
+    
+    str = "monitoredKeysStr len= "
+    print str
+    FILE_FOR_MORE_INFORMATION.write("%s\n" % str)
+    print len(monitoredKeysStr)
+    FILE_FOR_MORE_INFORMATION.write("%d\n" % (len(monitoredKeysStr)))
+    
+    
+    monitoredKeysStr = monitoredKeysStr[0:len(monitoredKeysStr)/1]
+    str = "new monitoredKeysStr len= "
+    print str
+    FILE_FOR_MORE_INFORMATION.write("%s\n" % str)
+    print len(monitoredKeysStr)
+    FILE_FOR_MORE_INFORMATION.write("%d\n" % (len(monitoredKeysStr)/1))
+    
+    
+    result = ComputeAvgOfAllMinumumsInMonitoredKeys(originalKeyStr, monitoredKeysStr)
+    
+    FILE_FOR_ALL_DICTS.close()
+    FILE_FOR_ALL_MINIMUM_DICTS.close()
+    FILE_FOR_MORE_INFORMATION.close()
 
 
 
